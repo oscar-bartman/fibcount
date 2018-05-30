374ec34da3c41cd2000e8cf14ed3eaa7f64279e9
oscar <oscar.bartman@gmail.com>
2018-05-30 09:59:12 +0200

    created a static grid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const values = [];
+const GRIDSIZE = 50;
+const CELLSIZE = 30;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      {/* Should we pass GRIDSIZE as a prop to Grid rather? */}
+        <Grid />
       </div>
     );
+  }
+}
+
+// COMPONENT
+class Grid extends Component {
+  constructor(props) {    
+    super(props)
+    initGrid()
+    //...
+  }
+
+  //...
+
+  render() {
+    const height = 50 * CELLSIZE 
+    const width = 50 * CELLSIZE
+    
+    return (
+      <svg height={height} width={width}>
+        {values.map((v, i) => 
+          <Cell index={i} key={i.toString()}/>
+        )}
+      </svg>
+    )
+  }
+}
+
+class Cell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: 0}
+  }
+
+  render() {
+    const index = this.props.index
+    const xPosition = (index % GRIDSIZE) * CELLSIZE
+    const yPosition = Math.floor(index/GRIDSIZE) * CELLSIZE
+
+    //TODO move styling to CSS
+    return <rect height={CELLSIZE} width={CELLSIZE} x = {xPosition} y = {yPosition} strokeWidth="2" stroke="#000000" fill="#FF0000"/>
+  }
+}
+
+//TODO move helpers to some util module we can import into here
+//HELPERS
+function initGrid() {
+  for (let i = 0; i < GRIDSIZE * GRIDSIZE; i++) {
+    values[i] = 0;
   }
 }
 
